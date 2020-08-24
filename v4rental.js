57369ced1b48c3a1fd8687be5b5615e72709f28e
Callum Watson <66056978+MrMullac@users.noreply.github.com>
2020-08-25 09:44:49 +1200

    Pogpogpog

--- v4rental.js
+++ v4rental.js
@@ -11,8 +11,6 @@
 //
 /********************************************************************/
 
-//<div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
-
 
 // Constants
 const form = document.getElementById('nameform'); // Getting the name form from html.
